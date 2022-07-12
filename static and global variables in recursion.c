d4371de5937cc641c9ef8108ae05062272c8e0d0
avniamitkumar <avni.shivi@gmail.com>
2022-07-12 23:50:12 +0530

    static and global variable

---
+++ static and global variables in recursion.c
@@ -0,0 +1,20 @@
+#include<iostream>
+#include <stdio.h>
+using namespace std;
+
+int fun(int n)
+{
+    if(n>0){
+        return fun(n-1)+n;
+    }
+    return 0;
+}
+
+int main()
+{
+    int r;
+    r=fun(5);
+    printf("%d",r);
+
+    return 0;
+}
