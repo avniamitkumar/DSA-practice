90c7bef3a29bb6fbd2d8d8755278de7ce33db946
avniamitkumar <avni.shivi@gmail.com>
2022-08-20 22:35:18 +0530

    ARRAY-1

---
+++ size increase of an dynamic array.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int *p,*q;
+    p= new int[5];
+    q=new int [10];
+
+    p[0]=3;
+    p[1]=5;
+    p[2]=7;
+    p[3]=9;
+    p[4]=11;
+
+    for(int i=0;i<5;i++)
+    {
+        cout<<p[i]<<" ";
+    }
+
+    cout<<endl;
+
+    for(int i=0;i<5;i++)
+    {
+        q[i]=p[i];
+    }
+
+    for(int i=0;i<5;i++)
+    {
+        cout<<q[i]<<" ";
+    }
+
+    delete []p;
+    p=q;
+    q=NULL;
+
+
+    return 0;
+}
